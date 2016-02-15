4c0ef4f7100cd8906f07afcf7608257205c4588d
Matteo Alessio Carrara <sw.matteoac@gmail.com>
2016-02-15 23:44:15 +0000

    Aggiornate informazioni sulla versione

--- src/version.py
+++ src/version.py
@@ -31,7 +31,7 @@ lib_name = "lib-figafind"
 # This project adheres to Semantic Versioning (http://semver.org/)
 
 version_major = 2
-version_minor = 0
+version_minor = 1
 version_patch = 0
 
 version_str = "%d.%d.%d" % (version_major, version_minor, version_patch)
